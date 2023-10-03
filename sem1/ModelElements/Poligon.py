cfb10b14efc5eba12178572145eaef0c44863ced
Danil <Kutfilloev.danil@yandex.ru>
2023-10-03 12:53:42 +0500

    задача первого семинара(сложно)

---
+++ sem1/ModelElements/Poligon.py
@@ -0,0 +1,8 @@
+from Point3D import Point3D
+
+
+class Poligon (Point3D):
+    point3d = Point3D
+
+    def __init__(self, point3d):
+        self.point3d = point3d
---
+++ sem1/ModelElements/Scene.py
@@ -0,0 +1,19 @@
+from PoligonalModel import PoligonalModel
+from Flash import Flash
+
+
+class Scene:
+    id: int
+    list.models = PoligonalModel
+    list.flashes = Flash
+
+    def __init__(self, id, models, flashes):
+        self.id = id
+        self.models = models
+        self.flashes = flashes
+
+    def Metod1(self, type1):
+        return type(type1)
+
+    def Metod2(self, type2):
+        return type(type2)
